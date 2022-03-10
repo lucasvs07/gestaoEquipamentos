cc723a2875c5155a760821110caa9eecd70e0021
lucasvs07 <lucasviniciusdasilva0@gmail.com>
2022-03-10 03:21:44 -0300

    Implementadas mais umas coisinhas.

--- gestaoEquipamentosV2.ConsoleApp/Program.cs
+++ gestaoEquipamentosV2.ConsoleApp/Program.cs
@@ -5,25 +5,29 @@ namespace gestaoDeEquipamentos.ConsoleApp
     internal class Program
     {
         static string[] nomeEquipamentos = new string[1000];
-        static string[] precoEquipamentos = new string[1000];
+        static decimal[] precoEquipamentos = new decimal[1000];
         static string[] numerodeSerie = new string[1000];
-        static string[] dataDeFabricacao = new string[1000];
+        static DateTime[] dataDeFabricacao = new DateTime[1000];
         static string[] fabricantes = new string[1000];
+
         static string[] equipamentosChamados = new string[1000];
         static string[] numeroDeChamados = new string[1000];
-        static string[] tituloDeChamados  = new string[1000];
+        static string[] tituloDeChamados = new string[1000];
         static string[] dataAberturaChamados = new string[1000];
         static string[] descricaoDeChamados = new string[1000];
+        
         static string[] nomeUsuarios = new string[1000];
         static string[] emailUsuarios = new string[1000];
         static string[] telefoneUsuarios = new string[1000];
+        static string[] nomeResponsaveisCadaChamado = new string[1000];
+        
 
 
 
 
         static int contadorEquipamentos = 0;
         static int contadorChamados = 0;
-        static int contadorUsuarios = 0;
+        //static int contadorUsuarios = 0;
 
         //método do primeiro menu
         static void Main(string[] args)
@@ -42,12 +46,12 @@ namespace gestaoDeEquipamentos.ConsoleApp
                     "1 para o menu de equipamentos.\n" +
                     "2 para o menu de manutenção.\n" +
                     "3 para o menu de solicitantes.\n" +
-                    "4 para sair.");
+                    "4 para sair.\n");
                 opcao = Console.ReadLine();
 
                 if (opcao == "1")
                 {
-                    equipamentos();
+                    menuEquipamentos();
                 }
                 else if (opcao == "2")
                 {
@@ -55,7 +59,7 @@ namespace gestaoDeEquipamentos.ConsoleApp
                 }
                 else if (opcao == "3")
                 {
-                    responsaveis();
+                    Console.WriteLine("responsaveis()");
                 }
                 else if (opcao == "4")
                 {
@@ -70,8 +74,10 @@ namespace gestaoDeEquipamentos.ConsoleApp
 
 
         //métodos para os equipamentos
-        static void equipamentos()
+        static void menuEquipamentos()
         {
+            Console.Clear();
+
             while (true)
             {
                 Console.Write("Digite a opção desejada:\n" +
@@ -86,40 +92,36 @@ namespace gestaoDeEquipamentos.ConsoleApp
                 switch (opcaoEquipammento)
                 {
                     case 1:
-                    //if (opcaoEquipammento == 1)
-                    
+
                         registrarEquipamento();
+
                         return;
-                    
 
                     case 2:
-                    //else if (opcaoEquipammento == 2)
-                    
-                        visualizarEquipamentos();
-                        return;
-                    
 
+                        visualizarEquipamentos();
+
+                        return;
 
                     case 3:
-                    //else if (opcaoEquipammento == 3)
-                    
-                        editarEquipamento();
-                        return;
-                    
 
+                        editarEquipamento();
+
+                        return;
 
                     case 4:
-                    //else if (opcaoEquipammento == 4)
-                    
-                        excluirEquipamento();
-                        return;
-                    
 
-                    //else if (opcaoEquipammento == 5)
+                        excluirEquipamento();
+
+
+                        return;
+
                     case 5:
-                       Console.Clear();
-                       menuPrincipal();
-                       break;
+
+                        Console.Clear();
+                        menuPrincipal();
+
+                        break;
                 }
             }
 
@@ -129,50 +131,112 @@ namespace gestaoDeEquipamentos.ConsoleApp
 
         static void registrarEquipamento()
         {
-            Console.WriteLine("Digite o nome do equipamento (min. 6 caracteres).");
-            string nome = Console.ReadLine();
-            nomeEquipamentos[contadorEquipamentos] = nome;
+            while (true)
+            {
+                Console.Clear();
 
-            Console.WriteLine("Digite o preço de aquisição desse equipamento.");
-            string preco = Console.ReadLine();
-            precoEquipamentos[contadorEquipamentos] = preco;
+                Console.WriteLine("Digite o nome do equipamento (min. 6 caracteres).");
+                string nome = Console.ReadLine();
 
-            Console.WriteLine("Cadastre o número de série do equipamento.");
-            string numDeSerieEquipamento = Console.ReadLine();
-            numerodeSerie[contadorEquipamentos] = numDeSerieEquipamento;
+                char[] nomeArray = nome.ToCharArray();
 
-            Console.WriteLine("Cadastre a data de fabricação do equipamento.");
-            string dataEquipamento = Console.ReadLine();
-            dataDeFabricacao[contadorEquipamentos] = dataEquipamento;
+                if (nomeArray.Length < 6)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
 
-            Console.WriteLine("Cadastre o fabricante do equipamento.");
-            string fabricanteEquipamento = Console.ReadLine();
-            fabricantes[contadorEquipamentos] = fabricanteEquipamento;
+                    Console.WriteLine("Nome de equipamento deve ter 6 caracteres.");
 
-            contadorEquipamentos++;
-            Console.Clear();
+                    Console.ResetColor();
 
-            equipamentos();
+                    Console.WriteLine("Pressione qualquer tecla para continuar.");
+                    Console.ReadKey();
+
+                    continue;
+                }
+
+                else
+                    nomeEquipamentos[contadorEquipamentos] = nome;
+                break;
+            }
+
+                Console.WriteLine("Cadastre o preço de aquisição desse equipamento.");
+                decimal preco = Convert.ToDecimal(Console.ReadLine());
+                precoEquipamentos[contadorEquipamentos] = preco;
+
+                Console.WriteLine("Cadastre o número de série do equipamento.");
+                string numDeSerieEquipamento = Console.ReadLine();
+                numerodeSerie[contadorEquipamentos] = numDeSerieEquipamento;
+
+                Console.WriteLine("Cadastre a data de fabricação do equipamento.");
+                DateTime dataEquipamento = Convert.ToDateTime(Console.ReadLine());
+                dataDeFabricacao[contadorEquipamentos] = dataEquipamento;
+
+                Console.WriteLine("Cadastre o fabricante do equipamento.");
+                string fabricanteEquipamento = Console.ReadLine();
+                fabricantes[contadorEquipamentos] = fabricanteEquipamento;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\nEquipamento " + numDeSerieEquipamento + " cadastrado com sucesso.");
+            Console.ResetColor();
+
+                contadorEquipamentos++;
+
+            while (true)
+            {
+                
+                Console.Write("\nDigite 1 para registrar um novo equipamento e 2 para voltar ao menu anterior.\n");
+                string opcao = Console.ReadLine();
+
+                if (opcao != "1" && opcao != "2")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.Write("\nOpção Inválida.");
+                    Console.ResetColor();
+
+                    Console.Write("\nPressione qualquer tecla para continuar.");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                else if (opcao == "1")
+                {
+                    registrarEquipamento();
+                    break;
+                }
+
+                else if (opcao == "2")
+                {
+                    Console.Clear();
+                    menuEquipamentos();
+                    break;
+                }
+                
+            }
+
         }
 
         static void visualizarEquipamentos()
         {
+            Console.Clear();
+
             for (int i = 0; i < contadorEquipamentos; i++)
             {
-                Console.WriteLine(tituloDeChamados[i] + " " + equipamentosChamados[i] + " "
-                    + dataAberturaChamados[i] + " " + descricaoDeChamados[i]);
+                Console.WriteLine(numerodeSerie[i] + " " + nomeEquipamentos[i] + " "
+                    + precoEquipamentos[i] + " " + dataDeFabricacao[i] + " " + fabricantes[i]);
             }
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Pressione qualquer tecla para continuar.");
             Console.ReadKey();
             Console.Clear();
-            chamados();
+            menuEquipamentos();
         }
 
         static void editarEquipamento()
         {
             Console.Clear();
+
             Console.WriteLine("Digite o número de série do equipamento você deseja editar:");
             string equipamentoParaEditar = Console.ReadLine();
 
@@ -180,12 +244,17 @@ namespace gestaoDeEquipamentos.ConsoleApp
             {
                 if (equipamentoParaEditar == numerodeSerie[i])
                 {
-                    Console.WriteLine(nomeEquipamentos[i] + " " + numerodeSerie[i] + " " 
+                    Console.WriteLine(numerodeSerie[i] + " " + nomeEquipamentos[i] + " "
                         + precoEquipamentos[i] + " " + dataDeFabricacao[i] + " " + fabricantes[i]);
                     Console.WriteLine();
                     while (true)
                     {
-                        Console.WriteLine("O que você deseja editar:");
+                        Console.Write("O que você deseja editar:\n" +
+                            "1 para editar o número de série do equipamento.\n" +
+                            "2 para editar o nome do equipamento.\n" +
+                            "3 para editar o preço.\n" +
+                            "4 para editar a data de fabricação.\n" +
+                            "5 para editar o fabricante.\n");
                         int itemParaEditar = Convert.ToInt16(Console.ReadLine());
 
                         switch (itemParaEditar)
@@ -202,12 +271,12 @@ namespace gestaoDeEquipamentos.ConsoleApp
                                 break;
                             case 3:
                                 Console.WriteLine("Digite o novo preço:");
-                                string precoNovo = Console.ReadLine();
+                                decimal precoNovo = Convert.ToDecimal(Console.ReadLine());
                                 precoEquipamentos[i] = precoNovo;
                                 break;
                             case 4:
                                 Console.WriteLine("Digite a nova data de fabricação:");
-                                string dataNova = Console.ReadLine();
+                                DateTime dataNova = Convert.ToDateTime(Console.ReadLine());
                                 dataDeFabricacao[i] = dataNova;
                                 break;
                             case 5:
@@ -215,29 +284,54 @@ namespace gestaoDeEquipamentos.ConsoleApp
                                 string fabricanteNovo = Console.ReadLine();
                                 fabricantes[i] = fabricanteNovo;
                                 break;
-                        }
-                        Console.WriteLine("Digite 1 para fazer uma nova edição neste equipamento.");
-                        Console.WriteLine("Digite 2 para editar outro equipamento.");
-                        Console.WriteLine("Digite 3 para voltar ao menu principal.");
-                        int opcaoMenuFinalEdicao = Convert.ToInt16(Console.ReadLine());
+                            default:
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write("\nOpção inválida, tente novamente.\n");
+                                Console.ResetColor();
+                                return;
 
-                        
-                        if (opcaoMenuFinalEdicao == 1)
-                            continue;
-                        
-                        else if (opcaoMenuFinalEdicao == 2)
+                        }
+                        //  break;
+
+                        // }
+                        while (true)
                         {
-                            editarEquipamento();
-                            break;
+                            Console.WriteLine();
+                            Console.WriteLine("Digite 1 para fazer uma nova edição neste equipamento.");
+                            Console.WriteLine("Digite 2 para editar outro equipamento.");
+                            Console.WriteLine("Digite 3 para voltar ao menu principal.");
+                            int opcaoMenuFinalEdicaoEquipamentos = Convert.ToInt16(Console.ReadLine());
+
+                            switch (opcaoMenuFinalEdicaoEquipamentos)
+                            {
+                                //if (opcaoMenuFinalEdicaoEquipamentos == 1)
+                                case 1:
+                                    continue;
+
+                                //else if (opcaoMenuFinalEdicaoEquipamentos == 2)
+                                case 2:
+                                    //{
+                                    editarEquipamento();
+                                    break;
+                                //}
+
+                                //else if (opcaoMenuFinalEdicaoEquipamentos == 3)
+                                case 3:
+                                    //{
+                                    menuPrincipal();
+                                    break;
+
+                                default:
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Opção inválida, tente novamente.");
+                                    Console.ResetColor();
+
+                                    break;
+                            }
                         }
 
-                        else if (opcaoMenuFinalEdicao == 3)
-                        {
-                            menuPrincipal();
-                            break;
-                        }
+                       
                     }
-
                 }
             }
         }
@@ -257,7 +351,7 @@ namespace gestaoDeEquipamentos.ConsoleApp
                     Array.Clear(fabricantes, i, 1);
                     Array.Clear(dataDeFabricacao, i, 1);
                 }
-                
+
             }
         }
 
@@ -266,6 +360,8 @@ namespace gestaoDeEquipamentos.ConsoleApp
         {
             while (true)
             {
+                Console.Clear();
+
                 Console.Write("Digite a opção desejada:\n" +
                     "1 para registrar um novo chamado.\n" +
                     "2 para visualizar os chamados abertos.\n" +
@@ -294,6 +390,8 @@ namespace gestaoDeEquipamentos.ConsoleApp
 
         static void registrarChamado()
         {
+            Console.Clear();
+
             Console.WriteLine("Registro de um novo chamado.");
             Console.WriteLine("Digite o nome do equipamento:");
             string nomeEquipamentoChamado = Console.ReadLine();
@@ -311,19 +409,27 @@ namespace gestaoDeEquipamentos.ConsoleApp
             string aberturaDeUmChamado = Console.ReadLine();
             dataAberturaChamados[contadorChamados] = aberturaDeUmChamado;
 
+            Console.WriteLine("Informe o nome responsável pela abertura do chamado:");
+            string responsavelChamado = Console.ReadLine();
+            nomeResponsaveisCadaChamado[contadorChamados] = responsavelChamado;
+
+
             contadorChamados++;
             Console.Clear();
 
             chamados();
-            
+
         }
 
         static void verChamados()
         {
+            Console.Clear();
+
             for (int i = 0; i < contadorChamados; i++)
             {
                 Console.WriteLine(tituloDeChamados[i] + " " + equipamentosChamados[i] + " "
-                    + dataAberturaChamados[i] + " " + descricaoDeChamados[i]);
+                    + dataAberturaChamados[i] + " " + descricaoDeChamados[i] + " " + nomeResponsaveisCadaChamado[i]);
+
             }
             Console.WriteLine();
             Console.WriteLine();
@@ -335,6 +441,8 @@ namespace gestaoDeEquipamentos.ConsoleApp
 
         static void deletarChamado()
         {
+            Console.Clear();
+
             Console.WriteLine("Digite o número do chamado para excluir.");
             string serieParaExcluir = Console.ReadLine();
 
@@ -357,10 +465,11 @@ namespace gestaoDeEquipamentos.ConsoleApp
         }
 
         //métodos para os responsáveis.
-
+        /*
         static void responsaveis()
         {
             Console.Clear();
+
             Console.Write("Digite a opção desejada.\n" +
                 "1 para cadastrar novo responsável.\n" +
                 "2 para visualizar a lista de responsáveis pela manutenção.\n" +
@@ -391,6 +500,8 @@ namespace gestaoDeEquipamentos.ConsoleApp
 
         static void cadastrarResponsavel()
         {
+            Console.Clear();
+
             Console.WriteLine("Digite o nome do novo usuário:");
             string nomeDeUsuario = Console.ReadLine();
             nomeUsuarios[contadorUsuarios] = nomeDeUsuario;
@@ -406,11 +517,13 @@ namespace gestaoDeEquipamentos.ConsoleApp
             Console.Clear();
 
             responsaveis();
-                    
+
         }
 
         static void visualizarListadeResponsaveis()
         {
+            Console.Clear();
+
             for (int i = 0; i < contadorUsuarios; i++)
             {
                 Console.WriteLine(nomeUsuarios[i] + " " + emailUsuarios[i] + " "
@@ -427,6 +540,7 @@ namespace gestaoDeEquipamentos.ConsoleApp
         static void editarResponsavel()
         {
             Console.Clear();
+
             Console.WriteLine("Digite o nome do usuário que você deseja editar:");
             string usuarioParaEditar = Console.ReadLine();
 
@@ -489,6 +603,8 @@ namespace gestaoDeEquipamentos.ConsoleApp
                 }
             }
         }
+
+        */
     }
 }
 
